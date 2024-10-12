4ac48daaecb9e91859c6dd7f90bbcd031fac0fce
Anshita Nagar <81310800+Anshita700@users.noreply.github.com>
2024-10-12 17:43:42 +0530

    Add files via upload

---
+++ RightAngleTraingle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class RightAngleTraingle
+{
+public static void main(String gg[])
+{
+int row=5;
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<=i;j++)
+{
+System.out.println("*");
+}
+}
+}
+}
