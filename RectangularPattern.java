5c29f036f99649c395671eeeba532675fd5b73df
Anshita Nagar <81310800+Anshita700@users.noreply.github.com>
2024-10-12 17:44:05 +0530

    Add files via upload

---
+++ RectangularPattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class RectangularPattern
+{
+public static void main(String gg[])
+{
+int row=5;
+
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<row;j++)
+{
+System.out.println("*");
+}
+System.out.print("");
+}
+}
+}
